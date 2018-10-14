866ab0a0f602a2d55602848296b08c33dc6e99a9
mlugo2 <marcolugo02@gmail.com>
2018-10-14 00:43:07 -0500

    Documenting gpio config header

--- src/inc/Gpio_cfg.h
+++ src/inc/Gpio_cfg.h
@@ -39,9 +39,9 @@
 */
 typedef enum
 {
-	GPIO_LOW,							// Defines digital state ground
-	GPIO_HIGH,						// Defines digital state power
-	GPIO_PIN_STATE_MAX		// Defines the maximum digital state
+	GPIO_LOW,							/**< Defines digital state ground 		*/
+	GPIO_HIGH,							/**< Defines digital state power  		*/
+	GPIO_PIN_STATE_MAX					/**< Defines the maximum digital state	*/
 } GpioPinState_t;
 
 /**
@@ -51,59 +51,59 @@ typedef enum
 */
 typedef enum
 {
-	PORTA_0,									/**< PORTA_0 */
-	PORTA_1,									/**< PORTA_1 */
-	PORTA_2,									/**< PORTA_2 */
-	PORTA_3,									/**< PORTA_3 */
-	PORTA_4,									/**< PORTA_4 */
-	PORTA_5,									/**< PORTA_5 */
-	PORTA_6,									/**< PORTA_6 */
-	PORTA_7,									/**< PORTA_7 */
+	PORTA_0,							/**< PORTA_0 */
+	PORTA_1,							/**< PORTA_1 */
+	PORTA_2,							/**< PORTA_2 */
+	PORTA_3,							/**< PORTA_3 */
+	PORTA_4,							/**< PORTA_4 */
+	PORTA_5,							/**< PORTA_5 */
+	PORTA_6,							/**< PORTA_6 */
+	PORTA_7,							/**< PORTA_7 */
 
-	PORTB_0,									/**< PORTB_0 */
-	PORTB_1,									/**< PORTB_1 */
-	PORTB_2,									/**< PORTB_2 */
-	PORTB_3,									/**< PORTB_3 */
-	PORTB_4,									/**< PORTB_4 */
-	PORTB_5,									/**< PORTB_5 */
-	PORTB_6,									/**< PORTB_6 */
-	PORTB_7,									/**< PORTB_7 */
+	PORTB_0,							/**< PORTB_0 */
+	PORTB_1,							/**< PORTB_1 */
+	PORTB_2,							/**< PORTB_2 */
+	PORTB_3,							/**< PORTB_3 */
+	PORTB_4,							/**< PORTB_4 */
+	PORTB_5,							/**< PORTB_5 */
+	PORTB_6,							/**< PORTB_6 */
+	PORTB_7,							/**< PORTB_7 */
 
-	PORTC_0,									/**< PORTC_0 */
-	PORTC_1,									/**< PORTC_1 */
-	PORTC_2,									/**< PORTC_2 */
-	PORTC_3,									/**< PORTC_3 */
-	PORTC_4,									/**< PORTC_4 */
-	PORTC_5,									/**< PORTC_5 */
-	PORTC_6,									/**< PORTC_6 */
-	PORTC_7,									/**< PORTC_7 */
+	PORTC_0,							/**< PORTC_0 */
+	PORTC_1,							/**< PORTC_1 */
+	PORTC_2,							/**< PORTC_2 */
+	PORTC_3,							/**< PORTC_3 */
+	PORTC_4,							/**< PORTC_4 */
+	PORTC_5,							/**< PORTC_5 */
+	PORTC_6,							/**< PORTC_6 */
+	PORTC_7,							/**< PORTC_7 */
 
-	PORTD_0,									/**< PORTD_0 */
-	PORTD_1,									/**< PORTD_1 */
-	PORTD_2,									/**< PORTD_2 */
-	PORTD_3,									/**< PORTD_3 */
-	PORTD_4,									/**< PORTD_4 */
-	PORTD_5,									/**< PORTD_5 */
-	PORTD_6,									/**< PORTD_6 */
-	PORTD_7,									/**< PORTD_7 */
+	PORTD_0,							/**< PORTD_0 */
+	PORTD_1,							/**< PORTD_1 */
+	PORTD_2,							/**< PORTD_2 */
+	PORTD_3,							/**< PORTD_3 */
+	PORTD_4,							/**< PORTD_4 */
+	PORTD_5,							/**< PORTD_5 */
+	PORTD_6,							/**< PORTD_6 */
+	PORTD_7,							/**< PORTD_7 */
 
-	PORTE_0,									/**< PORTE_0 */
-	PORTE_1,									/**< PORTE_1 */
-	PORTE_2,									/**< PORTE_2 */
-	PORTE_3,									/**< PORTE_3 */
-	PORTE_4,									/**< PORTE_4 */
-	PORTE_5,									/**< PORTE_5 */
-	PORTE_6,									/**< PORTE_6 */
-	PORTE_7,									/**< PORTE_7 */
+	PORTE_0,							/**< PORTE_0 */
+	PORTE_1,							/**< PORTE_1 */
+	PORTE_2,							/**< PORTE_2 */
+	PORTE_3,							/**< PORTE_3 */
+	PORTE_4,							/**< PORTE_4 */
+	PORTE_5,							/**< PORTE_5 */
+	PORTE_6,							/**< PORTE_6 */
+	PORTE_7,							/**< PORTE_7 */
 
-	PORTF_0,									/**< PORTF_0 */
-	PORTF_1,									/**< PORTF_1 */
-	PORTF_2,									/**< PORTF_2 */
-	PORTF_3,									/**< PORTF_3 */
-	PORTF_4,									/**< PORTF_4 */
-	PORTF_5,									/**< PORTF_5 */
-	PORTF_6,									/**< PORTF_6 */
-	PORTF_7,									/**< PORTF_7 */
+	PORTF_0,							/**< PORTF_0 */
+	PORTF_1,							/**< PORTF_1 */
+	PORTF_2,							/**< PORTF_2 */
+	PORTF_3,							/**< PORTF_3 */
+	PORTF_4,							/**< PORTF_4 */
+	PORTF_5,							/**< PORTF_5 */
+	PORTF_6,							/**< PORTF_6 */
+	PORTF_7,							/**< PORTF_7 */
 	GPIO_MAX_PIN_NUM					/**< MAX CHANNELS */
 } GpioChannel_t;
 
@@ -112,9 +112,9 @@ typedef enum
 */
 typedef enum
 {
-	GPIO,										/**< Pin works in GPIO mode */
-	PERIPH,										/**< Pin controlled by certain peripheral */
-	FUNCT_MAX									/**< MAX FUNCTIONS AVAILABLE */
+	GPIO,								/**< Pin works in GPIO mode */
+	PERIPH,								/**< Pin controlled by certain peripheral */
+	MAX_FUNCTION						/**< MAX FUNCTIONS AVAILABLE */
 } GpioFunction_t;
 
 /**
@@ -122,8 +122,8 @@ typedef enum
 */
 typedef enum
 {
-	INPUT,
-	OUTPUT,
+	INPUT,								/**< Pin is configured for input */
+	OUTPUT,								/**< Pin is configured for output */
 	MAX_DIRECTION
 } GpioDirection_t;
 
@@ -132,18 +132,18 @@ typedef enum
 */
 typedef enum
 {
-	DISABLE,
-	ENABLE,
+	DISABLE,							/**< Pin could be configured as analog */
+	ENABLE,								/**< Pin is configured to work digitally */
 	MAX_DIGITAL_FUNCT
 } GpioDigitalEn_t;
 
 /**
-*	Defines the possible trigger modes of an interrput
+*	Defines the possible trigger modes of an interrupt
 */
 typedef enum
 {
-	EDGE,
-	LEVEL,
+	EDGE,								/**< Falling/Rising edges are triggers for interrupt */
+	LEVEL,								/**< Stable signal triggers the interrupt */
 	MAX_TRIGGER_MODES
 } GpioSense_t;
 
@@ -152,8 +152,8 @@ typedef enum
 */
 typedef enum
 {
-	EVENT,
-	BOTH,
+	EVENT,								/**< Interrupt controlled by Event register */
+	BOTH,								/**< Interrupt triggered by both edges */
 	MAX_BOTH_EDGES_MODES
 } GpioBothEdges_t;
 
@@ -162,8 +162,8 @@ typedef enum
 */
 typedef enum
 {
-	FALLING,
-	RISING,
+	FALLING,							/**< Detects falling edges */
+	RISING,								/**< Detects rising edges */
 	MAX_EVENT_MODE
 } GpioEvent_t;
 
@@ -172,8 +172,8 @@ typedef enum
 */
 typedef enum
 {
-	NOFX,
-	CLEAR,
+	NOFX,								/**< Does nothing.. */
+	CLEAR,								/**< Clears corresponding ISR pins */
 	MAX_NUM
 } GpioClear_t;
 
@@ -182,8 +182,8 @@ typedef enum
 */
 typedef enum
 {
-	MASK,
-	UNMASK,
+	MASK,								/**< Interrupt is disabled */
+	UNMASK,								/**< Interrupt is enabled */
 	MAX_MASK_STATE
 } GpioMask_t;
 
